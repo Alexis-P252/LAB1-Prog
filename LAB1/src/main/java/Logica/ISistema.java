6ca37b159d3643c36ed36966e508b3ae5f5dfc85
Alexis252 <alexis.peralta@estudiantes.utec.edu.uy>
2021-08-16 16:21:00 -0300

    Primer Commit

---
+++ LAB1/src/main/java/Logica/ISistema.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+import Logica.DtFecha;
+/**
+ *
+ * @author User
+ */
+public interface ISistema {
+    public abstract void ingresarEspectador(String nombre, String apellido, String correo, String nickname, DtFecha fecha_nac);
+    public abstract void ingresarArtista(String nombre, String apellido, String correo, String nickname, DtFecha fecha_nac, String descripcion, String biografia, String link);
+    public abstract boolean UsuarioxNickname(String nickname);
+    public abstract boolean UsuarioxEmail(String email);
+    public abstract String[] ColNickname();
+    public abstract DtUsuario GetDtUsuario(String nickname);
+    
+}
---
+++ LAB1/src/main/java/Logica/Sistema.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import Logica.Usuario;
+import Logica.DtUsuario;
+import java.util.Date;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author User
+ */
+public class Sistema implements ISistema {
+    
+    private Map Usuarios;
+    
+    public Sistema(){
+        this.Usuarios = new HashMap();
+    }
+    
+    public void ingresarEspectador(String nombre, String apellido, String correo, String nickname, DtFecha fecha_nac){
+        
+        Usuario u = new Espectador(nombre, apellido, correo, nickname, fecha_nac);
+        this.Usuarios.put(nickname, u);
+    }
+    
+    public void ingresarArtista(String nombre, String apellido, String correo, String nickname, DtFecha fecha_nac, String descripcion, String biografia, String link){
+        
+        Usuario u = new Artista(nombre, apellido, correo, nickname, fecha_nac, biografia, descripcion, link);
+        this.Usuarios.put(nickname, u);
+    }
+    
+    public boolean UsuarioxNickname(String nickname){
+        if(this.Usuarios.get(nickname) == null)
+            return false;
+        else
+            return true;
+        
+    }
+    
+    public boolean UsuarioxEmail(String email){
+        
+        String s;
+        Iterator it = this.Usuarios.values().iterator();
+
+        while (it.hasNext()){
+            Usuario u = (Usuario) it.next();
+            s = u.GetEmail();
+            
+            if(s.equals(email))
+                return true;
+        }
+        return false;
+           
+    }
+    
+    public String[] ColNickname(){
+        String res[] = new String[this.Usuarios.size()];
+        
+        Iterator it = this.Usuarios.values().iterator();
+
+        int i = 0;
+        while (it.hasNext()){
+            Usuario u = (Usuario) it.next();
+            if(u instanceof Artista){
+                res[i] = u.GetNickname() + " (A)";
+            }
+            else{
+                res[i] = u.GetNickname() + " (E)";
+            }
+            i++;
+        }
+        
+        return res;
+    }
+    
+    public DtUsuario GetDtUsuario(String nickname){
+        Usuario u = (Usuario) this.Usuarios.get(nickname);
+        return u.ArmarDT();
+        
+    }
+    
+
+}
+
+
