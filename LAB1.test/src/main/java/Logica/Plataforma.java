84ca131e0cb9f01197a3bb354832cb18322adcd8
Nahu <contact@faztweb.com>
2021-08-19 09:43:18 -0300

    avance en consulta de espectaculo, uniendo lo que hizo andres

---
+++ LAB1.test/src/main/java/Logica/Plataforma.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+/**
+ *
+ * @author felip
+ */
+public class Plataforma {
+    private String nombre;
+    private String descripcion;
+    private String url;
+    private Map Espectaculos;
+
+    public Plataforma(String nombre, String descripcion, String url){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.url = url;
+        this.Espectaculos = new HashMap();
+    };
+
+    public String GetNombre(){
+      return this.nombre;
+    }
+
+     public String GetDescripcion(){
+      return this.descripcion;
+    }
+
+     public String GetUrl(){
+      return this.url;
+    }
+     
+     public void agregarEspectaculo(Espectaculo e){
+         Espectaculos.put(e.getNombre(),e);
+     }
+     
+     public boolean existeEspectaculo(String espectaculo){
+         if(this.Espectaculos.get(espectaculo)==null){
+             return false;
+         }else{
+             return true;
+         }         
+     }
+     
+     public String[] listarEspectaculoxPlataforma(){
+         String res[] = new String[this.Espectaculos.size()];
+        
+         Iterator it = this.Espectaculos.values().iterator();
+
+        int i = 0;
+        while (it.hasNext()){
+            Espectaculo e = (Espectaculo) it.next();
+            res[i] = e.getNombre();
+            i++;  
+        }
+        return res;
+     }
+     public DtEspectaculo retDtEspectaculo (String espectaculo){
+         Espectaculo e =  (Espectaculo) this.Espectaculos.get(espectaculo);
+         return e.crearDtEspectaculo();
+    }
+}
